d8df157026f13abbfea1c4bee4a6c13632868c2e
Nicholas Krämer <nicokraemer@gmx.net>
2021-12-07 15:17:37 +0100

    fonts

---
+++ momlplots/fonts.py
@@ -0,0 +1,18 @@
+
+def neurips(usetex=False):
+    # <- don't use LaTeX to typeset. It's much slower, and you can't change the font atm.
+    return {
+    "text.usetex": usetex,
+    "font.serif": ["Times New Roman"],
+    "mathtext.fontset": 'custom',
+    "mathtext.rm" : 'Times New Roman',
+    "mathtext.it" : "Times New Roman:italic",
+    "mathtext.bf" : 'Times New Roman:bold', #####
+    }
+
+
+def icml():
+    return {
+        "font.serif": ["Times"],
+        "mathtext.fontset": 'stix', # free ptmx replacement
+    }
---
+++ tests/test_fonts.py
@@ -0,0 +1,11 @@
+import pytest
+from momlplots import fonts
+
+def test_icml():
+    font = fonts.icml()
+    assert isinstance(font, dict)
+
+
+def test_neurips():
+    font = fonts.neurips()
+    assert isinstance(font, dict)
