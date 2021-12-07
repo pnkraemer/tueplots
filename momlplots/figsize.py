e7c748c1715e1aea1e76f0295db853c4f85c8277
Nicholas Krämer <nicokraemer@gmx.net>
2021-12-07 14:52:45 +0100

    jmlr figsize

---
+++ momlplots/figsize.py
@@ -0,0 +1,9 @@
+
+
+def jmlr():
+    width = 433.62
+    height = 0.5 * width
+    return (width, height)
+
+def golden_ratio():
+    return  (5. ** 0.5 - 1.) / 2.
--- tests/test_figsize.py
+++ tests/test_figsize.py
@@ -1,6 +1,12 @@
 
-import momlplots
+from momlplots import figsize
 
 
 def test_jmlr():
-    assert True
+
+    size = figsize.jmlr()
+    assert isinstance(size, tuple)
+
+def test_golden_ratio():
+    ratio = figsize.golden_ratio()
+    assert abs(ratio - 1/1.61) < 0.1, ratio
