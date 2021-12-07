2f7690a800974a507be4278ba5a3bda8ce564216
Nicholas Krämer <nicokraemer@gmx.net>
2021-12-07 15:02:35 +0100

    icml size

--- momlplots/figsize.py
+++ momlplots/figsize.py
@@ -1,9 +1,24 @@
 
 
 def jmlr():
+    """JMLR figure size """
     width = 433.62
-    height = 0.5 * width
-    return (width, height)
+    height = 0.5 * width * golden_ratio()
+    return width, height
+
+def icml(*, column="half"):
+    height_per_width = golden_ratio()
+    if column == "half":
+        width = 234.8775
+        height = height_per_width * width
+        return width, height
+    width = 487.8225
+    height = width * height_per_width / 2.0
+    return width, height
+
 
 def golden_ratio():
     return  (5. ** 0.5 - 1.) / 2.
+
+def inches_per_point():
+    return 1. / 72.27
--- tests/test_figsize.py
+++ tests/test_figsize.py
@@ -1,12 +1,24 @@
 
+import pytest
 from momlplots import figsize
 
 
-def test_jmlr():
 
+full_and_half_columns = pytest.mark.parametrize("column", ["full", "half"])
+
+@full_and_half_columns
+def test_icml(column):
+    size = figsize.icml(column=column)
+    assert isinstance(size, tuple)
+
+def test_jmlr():
     size = figsize.jmlr()
     assert isinstance(size, tuple)
 
 def test_golden_ratio():
     ratio = figsize.golden_ratio()
     assert abs(ratio - 1/1.61) < 0.1, ratio
+
+def test_inches_per_point():
+    in_per_pt = figsize.inches_per_point()
+    assert in_per_pt > 0.
