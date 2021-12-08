5d969fae2da1b9d92e4b34a84fe71d84cf1b3f2f
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2021-12-08 16:27:29 +0100

    moved behaviour from fonts to respective setups

--- tueplots/fonts.py
+++ tueplots/fonts.py
@@ -31,15 +31,6 @@ def beamer_moml():
     return {
         "font.serif": ["Roboto Condensed"],
         "font.family": "serif",
-        "text.color": colors.tue_dark(),
-        "axes.edgecolor": colors.tue_dark(),
-        "axes.labelcolor": colors.tue_dark(),
-        "xtick.color": colors.tue_dark(),
-        "ytick.color": colors.tue_dark(),
-        "axes.facecolor": "none",
-        "axes.titlesize": "medium",
-        "grid.color": colors.tue_gray(),
-        "axes.grid": False,
     }
 
 
@@ -48,10 +39,4 @@ def beamer_moml_dark_bg():
     return {
         "font.serif": ["Roboto Condensed"],
         "font.family": "serif",
-        "text.color": "w",
-        "axes.edgecolor": "w",
-        "axes.labelcolor": "w",
-        "xtick.color": "w",
-        "ytick.color": "w",
-        "axes.facecolor": "none",
     }
