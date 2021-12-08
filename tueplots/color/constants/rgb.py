126fc2ad6380f5652fc3f926a25f344f93827ed5
Philipp Hennig <phennig@tuebingen.mpg.de>
2021-12-08 22:11:41 +0100

    minor bug fix

--- tueplots/color/constants/rgb.py
+++ tueplots/color/constants/rgb.py
@@ -15,18 +15,18 @@ tue_gold = _np.array([174.0, 159.0, 109.0]) / 255.0
 tue_lightgold = _np.array([239.0, 236.0, 226.0]) / 255.0
 
 # secondary colors
-tue_secondary1 = _np.array([65,90,140]) / 255.0   % dark blue
-tue_secondary2 = _np.array([0,105,170]) / 255.0   % blue
-tue_secondary3 = _np.array([80,170,200]) / 255.0  % light blue 
-tue_secondary4 = _np.array([130,185,160]) / 255.0 % light green
-tue_secondary5 = _np.array([125,165,75]) / 255.0  % green
-tue_secondary6 = _np.array([50,110,30]) / 255.0   % dark green
-tue_secondary7 = _np.array([200,80,60]) / 255.0   % ocre 
-tue_secondary8 = _np.array([175,110,150]) / 255.0 % violet
-tue_secondary9 = _np.array([180,160,150]) / 255.0 % mauve
-tue_secondary10 = _np.array([215,180,105]) / 255.0% light orange
-tue_secondary11 = _np.array([210,150,0]) / 255.0  % orange
-tue_secondary12 = _np.array([145,105,70]) / 255.0 % brown
+tue_secondary1 = _np.array([65.0, 90.0, 140.0]) / 255.0  # dark blue
+tue_secondary2 = _np.array([0.0, 105.0, 170.0]) / 255.0  # blue
+tue_secondary3 = _np.array([80.0, 170.0, 200.0]) / 255.0  # light blue
+tue_secondary4 = _np.array([130.0, 185.0, 160.0]) / 255.0  # light green
+tue_secondary5 = _np.array([125.0, 165.0, 75.0]) / 255.0  # green
+tue_secondary6 = _np.array([50.0, 110.0, 30.0]) / 255.0  # dark green
+tue_secondary7 = _np.array([200.0, 80.0, 60.0]) / 255.0  # ocre
+tue_secondary8 = _np.array([175.0, 110.0, 150.0]) / 255.0  # violet
+tue_secondary9 = _np.array([180.0, 160.0, 150.0]) / 255.0  # mauve
+tue_secondary10 = _np.array([215.0, 180.0, 105.0]) / 255.0  # light orange
+tue_secondary11 = _np.array([210.0, 150.0, 0.0]) / 255.0  # orange
+tue_secondary12 = _np.array([145.0, 105.0, 70.0]) / 255.0  # brown
 
 # speaking aliases for the secondary colors
 tue_darkblue = tue_secondary1
