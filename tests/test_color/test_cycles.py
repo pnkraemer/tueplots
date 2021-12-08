1ac9b216afda91a4244fb21b07ff8ba74022287e
Nicholas Krämer <nicokraemer@gmx.net>
2021-12-08 08:27:25 +0100

    added high contrast

--- tests/test_color/test_cycles.py
+++ tests/test_color/test_cycles.py
@@ -3,7 +3,12 @@
 from tueplots.color import cycler
 import matplotlib.pyplot as plt
 
+
 def test_bright():
     cy = cycler.bright()
     plt.rcParams.update(cy)
 
+
+def test_high_contrast():
+    cy = cycler.high_contrast()
+    plt.rcParams.update(cy)
--- tueplots/color/cycler.py
+++ tueplots/color/cycler.py
@@ -1,7 +1,14 @@
-
 from matplotlib import cycler
+
 
 def bright():
     """From Paul Tot's website: https://personal.sron.nl/~pault/."""
-    color_cycler = cycler('color', ['4477AA', 'EE6677', '228833', 'CCBB44', '66CCEE', 'AA3377', 'BBBBBB'])
+    color_cycler = cycler(
+        "color", ["4477AA", "EE6677", "228833", "CCBB44", "66CCEE", "AA3377", "BBBBBB"]
+    )
+    return {"axes.prop_cycle": color_cycler}
+
+def high_contrast():
+    """From Paul Tot's website: https://personal.sron.nl/~pault/."""
+    color_cycler = cycler('color', ['004488', 'DDAA33', 'BB5566'])
     return {"axes.prop_cycle": color_cycler}
