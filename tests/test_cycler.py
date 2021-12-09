8e77e023e10abb1786b6214bbdc94e03940ba007
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2021-12-09 14:10:41 +0100

    marker into cycle tests

--- tests/test_cycler.py
+++ tests/test_cycler.py
@@ -3,9 +3,10 @@
 import matplotlib.pyplot as plt
 
 from tueplots import cycler
+from tueplots.constants import markers
 from tueplots.constants.color import palettes
 
 
 def test_cycler():
-    cy = cycler.cycler(color=palettes.tue_primary)
+    cy = cycler.cycler(color=palettes.tue_primary[:3], marker=markers.triangles[:3])
     plt.rcParams.update(cy)
