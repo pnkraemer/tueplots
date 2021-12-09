8234ea525d087f764cf47547411bb99ca354566f
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2021-12-09 14:49:50 +0100

    context are bundles now

--- tests/test_bundles.py
+++ tests/test_bundles.py
@@ -2,24 +2,24 @@
 
 import matplotlib.pyplot as plt
 
-from tueplots import context
+from tueplots import bundles
 
 
 def test_icml2022():
-    with context.icml2022():
+    with plt.rc_context(bundles.icml2022()):
         pass
 
 
 def test_neurips2021():
-    with context.neurips2021():
+    with plt.rc_context(bundles.neurips2021()):
         pass
 
 
 def test_beamer_moml():
-    with context.beamer_moml():
+    with plt.rc_context(bundles.beamer_moml()):
         pass
 
 
 def test_beamer_moml_dark_bg():
-    with context.beamer_moml_dark_bg():
+    with plt.rc_context(bundles.beamer_moml_dark_bg()):
         pass
--- tueplots/bundles.py
+++ tueplots/bundles.py
@@ -9,20 +9,20 @@ from tueplots.constants.color import rgb
 def icml2022():
     size = figsize.icml2022()
     font_config = fonts.icml2022()
-    return plt.rc_context({**font_config, "figure.figsize": size})
+    return {**font_config, "figure.figsize": size}
 
 
 def neurips2021():
     size = figsize.neurips2021()
     font_config = fonts.neurips2021()
-    return plt.rc_context({**font_config, "figure.figsize": size})
+    return {**font_config, "figure.figsize": size}
 
 
 def beamer_moml():
     size = figsize.beamer()
     font_config = fonts.beamer_moml()
     axes_config = axes.lines(color=rgb.tue_dark)
-    return plt.rc_context({**font_config, **axes_config, "figure.figsize": size})
+    return {**font_config, **axes_config, "figure.figsize": size}
 
 
 def beamer_moml_dark_bg():
@@ -30,6 +30,9 @@ def beamer_moml_dark_bg():
     font_config = fonts.beamer_moml_dark_bg()
     axes_config_line = axes.lines(color="w")
     axes_config_face = axes.face(color=rgb.tue_dark)
-    return plt.rc_context(
-        {**font_config, **axes_config_line, **axes_config_face, "figure.figsize": size}
-    )
+    return {
+        **font_config,
+        **axes_config_line,
+        **axes_config_face,
+        "figure.figsize": size,
+    }
