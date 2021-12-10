1f159b1b187aafdcaa91f4eb4ca644eeb13735d3
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2021-12-10 15:23:52 +0100

    neurips tex

--- tueplots/fonts.py
+++ tueplots/fonts.py
@@ -1,15 +1,33 @@
 """Font settings for conference papers and journals."""
 
 
-def neurips2021(*, usetex=False, family="serif"):
+def neurips2021(*, family="serif"):
     return {
-        "text.usetex": usetex,
+        "text.usetex": False,
         "font.serif": ["Times New Roman"],
         "mathtext.fontset": "custom",
         "mathtext.rm": "Times New Roman",
         "mathtext.it": "Times New Roman:italic",
         "mathtext.bf": "Times New Roman:bold",
         "font.family": family,
+    }
+
+
+def neurips2021_tex(*, family="serif"):
+    preamble = r"\renewcommand{\rmdefault}{ptm}\renewcommand{\sfdefault}{phv}"
+    if family == "serif":
+        return {
+            "text.usetex": True,
+            "font.family": "serif",
+            "text.latex.preamble": preamble,
+        }
+    preamble += (
+        r"\renewcommand{\familydefault}{\sfdefault} \usepackage{sansmath} \sansmath"
+    )
+    return {
+        "text.usetex": True,
+        "font.family": "sans-serif",
+        "text.latex.preamble": preamble,
     }
 
 
@@ -26,9 +44,7 @@ def icml2022(*, family="serif"):
 
 
 def icml2022_tex(*, family="serif"):
-
     preamble = r"\usepackage{times} "
-
     if family == "serif":
         return {
             "text.usetex": True,
@@ -48,6 +64,7 @@ def icml2022_tex(*, family="serif"):
 def beamer_moml():
     """For use with the MoML beamer template."""
     return {
+        "text.usetex": False,
         "font.serif": ["Roboto Condensed"],
         "font.family": "serif",
     }
@@ -56,6 +73,7 @@ def beamer_moml():
 def beamer_moml_dark_bg():
     """Colors for dark beamer slides."""
     return {
+        "text.usetex": False,
         "font.serif": ["Roboto Condensed"],
         "font.family": "serif",
     }
