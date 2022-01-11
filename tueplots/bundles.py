f2b4b109578799a27ffa2f150bbf055665c6a5af
Nicholas Krämer <nicokraemer@gmx.net>
2022-01-11 10:13:29 +0100

    figsize -> figsizes

--- tueplots/bundles.py
+++ tueplots/bundles.py
@@ -1,46 +1,46 @@
 """Context managers for plots."""
 
-from tueplots import axes, cycler, figsize, fonts, fontsizes
+from tueplots import axes, cycler, figsizes, fonts, fontsizes
 from tueplots.constants.color import palettes, rgb
 
 
 def icml2022(*, column="half", nrows=1, family="sans-serif"):
-    size = figsize.icml2022(column=column, nrows=nrows)
+    size = figsizes.icml2022(column=column, nrows=nrows)
     font_config = fonts.icml2022(family=family)
     fontsize_config = fontsizes.icml2022()
     return {**font_config, **size, **fontsize_config}
 
 
 def icml2022_tex(*, column="half", nrows=1, family="sans-serif"):
-    size = figsize.icml2022(column=column, nrows=nrows)
+    size = figsizes.icml2022(column=column, nrows=nrows)
     font_config = fonts.icml2022_tex(family=family)
     fontsize_config = fontsizes.icml2022()
     return {**font_config, **size, **fontsize_config}
 
 
 def jmlr2001_tex(*, nrows=1, family="sans-serif"):
-    size = figsize.jmlr2001(nrows=nrows)
+    size = figsizes.jmlr2001(nrows=nrows)
     font_config = fonts.jmlr2001_tex(family=family)
     fontsize_config = fontsizes.jmlr2001()
     return {**font_config, **size, **fontsize_config}
 
 
 def neurips2021(*, nrows=1, family="sans-serif"):
-    size = figsize.neurips2021(nrows=nrows)
+    size = figsizes.neurips2021(nrows=nrows)
     font_config = fonts.neurips2021(family=family)
     fontsize_config = fontsizes.neurips2021()
     return {**font_config, **size, **fontsize_config}
 
 
 def neurips2021_tex(*, nrows=1, family="sans-serif"):
-    size = figsize.neurips2021(nrows=nrows)
+    size = figsizes.neurips2021(nrows=nrows)
     font_config = fonts.neurips2021_tex(family=family)
     fontsize_config = fontsizes.neurips2021()
     return {**font_config, **size, **fontsize_config}
 
 
 def beamer_moml(*, rel_width=1, rel_height=1):
-    size = figsize.beamer(rel_width=rel_width, rel_height=rel_height)
+    size = figsizes.beamer(rel_width=rel_width, rel_height=rel_height)
     font_config = fonts.beamer_moml()
     axes_config_line = axes.lines()
     axes_config_grid = axes.grid()
@@ -59,7 +59,7 @@ def beamer_moml(*, rel_width=1, rel_height=1):
 
 
 def beamer_moml_dark_bg(*, rel_width=1, rel_height=1):
-    size = figsize.beamer(rel_width=rel_width, rel_height=rel_height)
+    size = figsizes.beamer(rel_width=rel_width, rel_height=rel_height)
     font_config = fonts.beamer_moml_dark_bg()
     axes_config_line = axes.lines()
     axes_config_grid = axes.grid()
