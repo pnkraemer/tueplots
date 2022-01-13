bedace0207c4c844038f35eeee39c847c1270398
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2022-01-13 16:08:00 +0100

    fontsize test cases

---
+++ tests/rc_params_cases/fontsizes.py
@@ -0,0 +1,19 @@
+"""Test cases for fontsizes."""
+
+from tueplots import fontsizes
+
+
+def case_icml2022():
+    return fontsizes.icml2022()
+
+
+def case_neurips2021():
+    return fontsizes.neurips2021()
+
+
+def case_jmlr2001():
+    return fontsizes.jmlr2001()
+
+
+def case_beamer_moml():
+    return fontsizes.beamer_moml()
--- tests/test_rc_params.py
+++ tests/test_rc_params.py
@@ -9,6 +9,7 @@ CASE_MODULES = (
     ".rc_params_cases.bundles",
     ".rc_params_cases.cycler",
     ".rc_params_cases.figsizes",
+    ".rc_params_cases.fontsizes",
 )
 
 
