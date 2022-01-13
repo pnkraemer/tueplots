7f927d4279a7ee2bc2504b1e179d7255826f91c0
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2022-01-13 16:27:51 +0100

    cases for some constants

---
+++ tests/test_constants/test_arrays.py
@@ -0,0 +1,15 @@
+"""Array-type tests."""
+
+import numpy as np
+import pytest_cases
+
+CASE_MODULES = (
+    ".test_arrays_cases.case_markers",
+    ".test_arrays_cases.color_cases.case_palettes",
+)
+
+
+@pytest_cases.parametrize_with_cases("arr", cases=CASE_MODULES)
+def test_is_array(arr):
+    """Assert compatibiity with matplotlib.pyplot.rcParams.update()."""
+    assert isinstance(arr, np.ndarray)
--- tueplots/constants/color/palettes.py
+++ tueplots/constants/color/palettes.py
@@ -1,90 +1,105 @@
 """Color palettes."""
 
+import numpy as np
+
 from tueplots.constants.color import rgb
 
-tue_primary = (
-    rgb.tue_red,
-    rgb.tue_dark,
-    rgb.tue_gray,
-    rgb.tue_gold,
-    rgb.tue_lightgold,
+tue_primary = np.array(
+    [
+        rgb.tue_red,
+        rgb.tue_dark,
+        rgb.tue_gray,
+        rgb.tue_gold,
+        rgb.tue_lightgold,
+    ]
 )
 
-tue_secondary = (
-    rgb.tue_darkblue,
-    rgb.tue_blue,
-    rgb.tue_lightblue,
-    rgb.tue_lightgreen,
-    rgb.tue_green,
-    rgb.tue_darkgreen,
-    rgb.tue_ocre,
-    rgb.tue_violet,
-    rgb.tue_mauve,
-    rgb.tue_lightorange,
-    rgb.tue_orange,
-    rgb.tue_brown,
+tue_secondary = np.array(
+    [
+        rgb.tue_darkblue,
+        rgb.tue_blue,
+        rgb.tue_lightblue,
+        rgb.tue_lightgreen,
+        rgb.tue_green,
+        rgb.tue_darkgreen,
+        rgb.tue_ocre,
+        rgb.tue_violet,
+        rgb.tue_mauve,
+        rgb.tue_lightorange,
+        rgb.tue_orange,
+        rgb.tue_brown,
+    ]
 )
 
-tue_plot = (
-    rgb.tue_red,
-    rgb.tue_dark,
-    rgb.tue_gold,
-    rgb.tue_blue,
-    rgb.tue_gray,
-    rgb.tue_green,
-    rgb.tue_ocre,
-    rgb.tue_violet,
-    rgb.tue_mauve,
-    rgb.tue_brown,
+tue_plot = np.array(
+    [
+        rgb.tue_red,
+        rgb.tue_dark,
+        rgb.tue_gold,
+        rgb.tue_blue,
+        rgb.tue_gray,
+        rgb.tue_green,
+        rgb.tue_ocre,
+        rgb.tue_violet,
+        rgb.tue_mauve,
+        rgb.tue_brown,
+    ]
 )
 """A mix of the primary and secondary colors from tue TUE color palette(s), ordered in a way that provides okay(ish) color contrast."""
 
-tue_plot_dark_bg = (
-    rgb.tue_lightgold,
-    rgb.tue_red,
-    rgb.tue_lightblue,
-    rgb.tue_gray,
-    rgb.tue_lightgreen,
-    rgb.tue_ocre,
-    rgb.tue_violet,
-    rgb.tue_mauve,
-    rgb.tue_brown,
+tue_plot_dark_bg = np.array(
+    [
+        rgb.tue_lightgold,
+        rgb.tue_red,
+        rgb.tue_lightblue,
+        rgb.tue_gray,
+        rgb.tue_lightgreen,
+        rgb.tue_ocre,
+        rgb.tue_violet,
+        rgb.tue_mauve,
+        rgb.tue_brown,
+    ]
 )
 """A mix of the primary and secondary colors from tue TUE color palette(s), ordered in a way that provides okay(ish) color contrast when plotting on dark background."""
 
-pn = (
-    rgb.pn_blue,
-    rgb.pn_green,
-    rgb.pn_orange,
-    rgb.pn_gray,
-    rgb.pn_red,
+pn = np.array(
+    [
+        rgb.pn_blue,
+        rgb.pn_green,
+        rgb.pn_orange,
+        rgb.pn_gray,
+        rgb.pn_red,
+    ]
 )
 
-bright = (
-    "4477AA",
-    "EE6677",
-    "228833",
-    "CCBB44",
-    "66CCEE",
-    "AA3377",
-    "BBBBBB",
+bright = np.array(
+    [
+        "4477AA",
+        "EE6677",
+        "228833",
+        "CCBB44",
+        "66CCEE",
+        "AA3377",
+        "BBBBBB",
+    ]
 )
 """From Paul Tot's website: https://personal.sron.nl/~pault/."""
 
-high_contrast = ("004488", "DDAA33", "BB5566")
+high_contrast = np.array(["004488", "DDAA33", "BB5566"])
 """From Paul Tot's website: https://personal.sron.nl/~pault/."""
 
-muted = (
-    "CC6677",
-    "332288",
-    "DDCC77",
-    "117733",
-    "88CCEE",
-    "882255",
-    "44AA99",
-    "999933",
-    "AA4499",
-    "DDDDDD",
+muted = np.array(
+    [
+        "CC6677",
+        "332288",
+        "DDCC77",
+        "117733",
+        "88CCEE",
+        "882255",
+        "44AA99",
+        "999933",
+        "AA4499",
+        "DDDDDD",
+    ]
 )
-
 """From Paul Tot's website: https://personal.sron.nl/~pault/."""
