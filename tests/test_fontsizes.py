7e80756ef3bb85e1596d02330a2ce8549b2d2d3d
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2022-01-13 16:05:25 +0100

    fontsizes.from_base is private now

--- tests/test_fontsizes.py
+++ tests/test_fontsizes.py
@@ -13,18 +13,3 @@ def test_icml2022():
 def test_neurips2021():
     sizes = fontsizes.icml2022()
     assert isinstance(sizes, dict)
-
-
-@pytest.mark.parametrize("base", [9, 10])
-def test_from_base(base):
-    r"""
-    Turn a base-size into font-sizes.
-
-    For a paper that uses size 10pt fonts for \normalsize, do
-    7pt for \scriptsize and
-    9pt for \footnotesize and
-    9pt for \small
-    Figure captions are set in \small, so that's what we're using here.
-    """
-    sizes = fontsizes.from_base(base=base)
-    assert isinstance(sizes, dict)
--- tueplots/fontsizes.py
+++ tueplots/fontsizes.py
@@ -2,22 +2,22 @@
 
 
 def icml2022():
-    return from_base(base=10)
+    return _from_base(base=10)
 
 
 def neurips2021():
-    return from_base(base=10)
+    return _from_base(base=10)
 
 
 def jmlr2001():
-    return from_base(base=10.95)
+    return _from_base(base=10.95)
 
 
 def beamer_moml():
-    return from_base(base=10)
+    return _from_base(base=10)
 
 
-def from_base(*, base=10):
+def _from_base(*, base=10):
     return {
         "font.size": base - 1,
         "axes.labelsize": base - 1,
