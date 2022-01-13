5f3f7ea8986a5c18ff16f98b69b7b364105b2038
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2022-01-13 15:47:18 +0100

    doc improvement

--- tests/test_rc_params.py
+++ tests/test_rc_params.py
@@ -8,10 +8,12 @@ CASE_MODULES = (".rc_params_cases.fonts", ".rc_params_cases.axes")
 
 @pytest_cases.parametrize_with_cases("config", cases=CASE_MODULES)
 def test_update_rcParams(config):
+    """Assert compatibiity with matplotlib.pyplot.rcParams.update()."""
     plt.rcParams.update(config)
 
 
 @pytest_cases.parametrize_with_cases("config", cases=CASE_MODULES)
 def test_rc_context(config):
+    """Assert compatibiity with matplotlib.pyplot.rc_context()."""
     with plt.rc_context(config):
         pass
