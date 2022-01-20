a0e41a75317faf923895bb2e010cb3b1cf4e0aa6
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2022-01-20 13:22:02 +0100

    removed *custom() test cases

--- tests/test_rc_params_cases/case_bundles.py
+++ tests/test_rc_params_cases/case_bundles.py
@@ -3,65 +3,33 @@
 from tueplots import bundles
 
 
-def case_bundles_icml2022_default():
-    return bundles.icml2022()
-
-
-def case_bundles_icml2022_custom():
+def case_bundles_icml2022():
     return bundles.icml2022(nrows=2, ncols=2, family="serif", column="full")
 
 
-def case_bundles_icml2022_tex_default():
-    return bundles.icml2022_tex()
-
-
-def case_bundles_icml2022_tex_custom():
+def case_bundles_icml2022_tex():
     return bundles.icml2022_tex(nrows=2, ncols=2, family="serif", column="full")
 
 
-def case_bundles_jmlr2001_tex_default():
-    return bundles.jmlr2001_tex()
-
-
-def case_bundles_jmlr2001_tex_custom():
+def case_bundles_jmlr2001_tex():
     return bundles.jmlr2001_tex(nrows=2, ncols=2, family="serif")
 
 
-def case_bundles_aistats2022_tex_default():
-    return bundles.aistats2022_tex()
-
-
-def case_bundles_aistats2022_tex_custom():
+def case_bundles_aistats2022_tex():
     return bundles.aistats2022_tex(column="full", nrows=2, ncols=2, family="serif")
 
 
-def case_bundles_neurips2021_default():
-    return bundles.neurips2021()
-
-
-def case_bundles_neurips2021_custom():
+def case_bundles_neurips2021():
     return bundles.neurips2021(nrows=2, ncols=2, family="serif")
 
 
-def case_bundles_neurips2021_tex_default():
-    return bundles.neurips2021_tex()
-
-
-def case_bundles_neurips2021_tex_custom():
+def case_bundles_neurips2021_tex():
     return bundles.neurips2021_tex(nrows=2, ncols=2, family="serif")
 
 
-def case_bundles_beamer_moml_default():
-    return bundles.beamer_moml()
-
-
-def case_bundles_beamer_moml_tex_custom():
+def case_bundles_beamer_moml_tex():
     return bundles.beamer_moml(rel_width=0.9, rel_height=0.9)
 
 
-def case_bundles_beamer_moml_dark_bg_default():
-    return bundles.beamer_moml()
-
-
-def case_bundles_beamer_moml_dark_bg_tex_custom():
+def case_bundles_beamer_moml_dark_bg_tex():
     return bundles.beamer_moml(rel_width=0.9, rel_height=0.9)
--- tests/test_rc_params_cases/case_figsizes.py
+++ tests/test_rc_params_cases/case_figsizes.py
@@ -4,51 +4,27 @@
 from tueplots import figsizes
 
 
-def case_figsizes_icml2022_default():
-    return figsizes.icml2022()
-
-
-def case_figsizes_icml2022_custom():
+def case_figsizes_icml2022():
     return figsizes.icml2022(column="full", nrows=2, ncols=3, height_to_width_ratio=1.0)
 
 
-def case_figsizes_aistats2022_default():
-    return figsizes.aistats2022()
-
-
-def case_figsizes_aistats2022_custom():
+def case_figsizes_aistats2022():
     return figsizes.aistats2022(
         column="full", nrows=2, ncols=3, height_to_width_ratio=1.0
     )
 
 
-def case_figsizes_cvpr2022_default():
-    return figsizes.cvpr2022()
-
-
-def case_figsizes_cvpr2022_custom():
+def case_figsizes_cvpr2022():
     return figsizes.cvpr2022(column="full", nrows=2, ncols=3, height_to_width_ratio=1.0)
 
 
-def case_figsizes_neurips2021_default():
-    return figsizes.neurips2021()
-
-
-def case_figsizes_neurips2021_custom():
+def case_figsizes_neurips2021():
     return figsizes.neurips2021(nrows=2, ncols=3, height_to_width_ratio=1.0)
 
 
-def case_figsizes_jmlr2001_default():
-    return figsizes.jmlr2001()
-
-
-def case_figsizes_jmlr2001_custom():
+def case_figsizes_jmlr2001():
     return figsizes.jmlr2001(nrows=2, ncols=3, height_to_width_ratio=1.0)
 
 
-def case_beamer_169_default():
-    return figsizes.beamer_169()
-
-
-def case_beamer_169_custom():
+def case_beamer_169():
     return figsizes.beamer_169(rel_width=0.5, rel_height=0.1)
