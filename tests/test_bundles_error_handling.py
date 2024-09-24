cd7dc6f3bdf9b547c4303b8fb19ea3993a13ca6f
Nicholas Krämer <nicokraemer@gmx.net>
2024-09-24 08:46:06 +0200

    Implement the multicol test-case via tags to enable reusing them for tex-or-not tests

--- tests/test_bundles_error_handling.py
+++ tests/test_bundles_error_handling.py
@@ -6,43 +6,51 @@ import pytest_cases
 from tueplots import bundles
 
 
-def case_multicol_bundle_cvpr2024():
+@pytest_cases.case(tags=["multicol"])
+def case_bundle_cvpr2024():
     return bundles.cvpr2024
 
 
-def case_multicol_bundle_icml2022():
+@pytest_cases.case(tags=["multicol"])
+def case_bundle_icml2022():
     return bundles.icml2022
 
 
-def case_multicol_bundle_icml2024():
+@pytest_cases.case(tags=["multicol"])
+def case_bundle_icml2024():
     return bundles.icml2024
 
 
-def case_multicol_bundle_aistats2022():
+@pytest_cases.case(tags=["multicol"])
+def case_bundle_aistats2022():
     return bundles.aistats2022
 
 
-def case_multicol_bundle_aistats2023():
+@pytest_cases.case(tags=["multicol"])
+def case_bundle_aistats2023():
     return bundles.aistats2023
 
 
-def case_multicol_bundle_aistats2025():
+@pytest_cases.case(tags=["multicol"])
+def case_bundle_aistats2025():
     return bundles.aistats2025
 
 
-def case_multicol_bundle_aaai2024():
+@pytest_cases.case(tags=["multicol"])
+def case_bundle_aaai2024():
     return bundles.aaai2024
 
 
-def case_multicol_bundle_uai2024():
+@pytest_cases.case(tags=["multicol"])
+def case_bundle_uai2024():
     return bundles.uai2023
 
 
-@pytest_cases.parametrize_with_cases("multicol_bundle", cases=".")
-def test_column_neither_half_nor_full_raises_value_error(multicol_bundle):
+@pytest_cases.parametrize_with_cases("bundle_fun", cases=".", has_tag=["multicol"])
+def test_column_neither_half_nor_full_raises_value_error(bundle_fun):
     # Sanity checks
-    _ = multicol_bundle(column="half")
-    _ = multicol_bundle(column="full")
+    _ = bundle_fun(column="half")
+    _ = bundle_fun(column="full")
 
     with pytest.raises(ValueError, match="expected"):
-        _ = multicol_bundle(column="anything-but-half")
+        _ = bundle_fun(column="anything-but-half")
